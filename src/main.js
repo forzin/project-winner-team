249aeb1c421a0c756aca8d96d045268b3547128b
Svitla-Dan <165526274+Svitla-Dan@users.noreply.github.com>
2024-05-26 21:11:38 +0200

    Add mobile menu markup and styles, script for toogling class is-open

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-import './js/testimonials.js'
+import './js/testimonials.js';
+
+(() => {
+  const mobileMenu = {
+    modal: document.querySelector('#backdrop'),
+    // menuOpenBtn: document.querySelector('.button-menu-open'),
+    menuCloseBtn: document.querySelector('.close-button'),
+    menuCloseByLink: document.querySelectorAll('.modal-menu-item-link'),
+  };
+//   mobileMenu.menuOpenBtn.addEventListener('click', toogleIsOpen);
+  mobileMenu.menuCloseBtn.addEventListener('click', toogleIsOpen);
+  mobileMenu.menuCloseByLink.forEach(link => {
+    link.addEventListener('click', toogleIsOpen);
+  });
+  function toogleIsOpen() {
+    mobileMenu.modal.classList.toggle('is-open');
+  }
+})();
